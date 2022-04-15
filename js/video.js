6d43755a6e48ba64306504262cc7d1f03be521fb
DAN <48164561+TAMAJJJ@users.noreply.github.com>
2022-04-14 21:44:07 -0400

    init

---
+++ js/video.js
@@ -0,0 +1,66 @@
+var video;
+
+window.addEventListener("load", function () {
+  console.log("Good job opening the window");
+  video = document.querySelector("video");
+  video.removeAttribute("autoplay");
+  video.removeAttribute("loop");
+});
+
+document.querySelector("#play").addEventListener("click", function () {
+  console.log("Play Video");
+  //   video.volume = document.getElementById("slider").value;
+  video.play();
+  document.querySelector("#volume").innerHTML = video.volume * 100 + "%";
+});
+
+document.querySelector("#pause").addEventListener("click", function () {
+  console.log("Pause Video");
+  video.pause();
+});
+
+document.querySelector("#slower").addEventListener("click", function () {
+  video.playbackRate *= 0.95;
+  console.log("New speed is " + video.playbackRate);
+});
+
+document.querySelector("#faster").addEventListener("click", function () {
+  video.playbackRate *= 1.05;
+  console.log("New speed is " + video.playbackRate);
+});
+
+document.querySelector("#skip").addEventListener("click", function () {
+  console.log("Original location " + video.currentTime);
+  video.currentTime += 15;
+  if (video.currentTime >= video.duration) {
+    video.currentTime = 0;
+  }
+  console.log("New location " + video.currentTime);
+});
+
+document.querySelector("#mute").addEventListener("click", function () {
+  const btn = document.getElementById("mute");
+  video.muted = !video.muted;
+  if (video.muted) {
+    btn.innerHTML = "Unmute";
+  } else {
+    btn.innerHTML = "Mute";
+  }
+});
+
+document.querySelector("#slider").addEventListener("click", function () {
+  console.log("Changing Volume");
+  console.log(this.value);
+  video.volume = this.value / 100;
+  document.querySelector("#volume").innerHTML = video.volume * 100 + "%";
+});
+
+document.querySelector("#vintage").addEventListener("click", function () {
+  console.log("Add Old School Style");
+  video.classList.add("oldSchool");
+});
+
+document.querySelector("#orig").addEventListener("click", function () {
+  console.log("Remove Old School Style");
+  video.classList.remove("oldSchool");
+});
